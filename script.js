e8eb3cf9251efc317aab177310a7358b2a8620bc
iNiloowf <74246387+iNiloowf@users.noreply.github.com>
2023-01-14 13:16:51 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['Correct', 3],
+  [true, 'Correct ✔'],
+  [false, 'Try Again 🤨'],
+]);
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key} : ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(question.get(question.get('Correct') === answer));
